92785df599004713ff5ab5e5bffb86e5e8dd8870
TylerBiggert <TylerB.Other@gmail.com>
2019-12-09 22:47:51 -0500

    Added providers routerLink to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProvidersComponent } from './providers/providers.component';
 import { ProvidersService } from './providers.service';
 import { PeakHeaderComponent } from './peak-header/peak-header.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ //registers component in this module
 	AppComponent,
 	ProvidersComponent,
-	PeakHeaderComponent //registers component in this module
+	PeakHeaderComponent,
+	HomeComponent,
+	NotFoundComponent
   ],
   imports: [
-    BrowserModule
+	BrowserModule,
+	RouterModule.forRoot([// define root route for app
+		{	path: '', 
+			component: HomeComponent
+		},
+		{
+			path: 'providers', 
+			component: ProvidersComponent
+		},
+		{
+			path: '**', //wild card catches any URL
+			component: NotFoundComponent
+		}
+	]
+	) 
   ],
   providers: [
 	  ProvidersService //dependencies
